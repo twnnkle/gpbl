761ff70e152a2ce679b9e0df70e565fd452a8316
twnnkle <twinkle@DESKTOP-AIB6J61>
2024-10-07 15:44:05 +0300

    Тест

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import {defineConfig} from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://twnnkle.github.io',
+    base: 'gpbl',
     output: 'static',
     build: {
         inlineStylesheets: 'never'
