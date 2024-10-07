a5d6d0b9f6da31630cd66bf19afe7f2e68836e2f
twnnkle <twinkle@DESKTOP-AIB6J61>
2024-10-07 17:24:20 +0300

    Тест

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import {defineConfig} from 'astro/config';
+import {passthroughImageService} from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://twnnkle.github.io',
+    image: {
+        service: passthroughImageService(),
+    },
     base: 'gpbl',
     output: 'static',
     build: {
