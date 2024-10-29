5af0895be2dbe22e18a5507c42c6ba5a408f76ab
twnnkle <twinkle@DESKTOP-AIB6J61>
2024-10-29 17:29:51 +0300

    Верстка новых страниц, поправил билд

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,43 @@
-import {defineConfig} from 'astro/config';
-import {passthroughImageService} from 'astro/config';
+import { defineConfig } from 'astro/config';
+import { passthroughImageService } from 'astro/config';
+import relativeLinks from 'astro-relative-links';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://twnnkle.github.io',
-    image: {
-        service: passthroughImageService(),
-    },
-    base: 'gpbl',
-    output: 'static',
+  //   site: 'https://twnnkle.github.io',
+  image: {
+    service: passthroughImageService(),
+  },
+  //   base: 'gpbl',
+  output: 'static',
+  trailingSlash: 'never',
+  build: {
+    inlineStylesheets: 'never',
+    assets: 'assets',
+    format: 'file',
+  },
+  integrations: [relativeLinks()],
+  vite: {
     build: {
-        inlineStylesheets: 'never'
+      cssMinify: false,
+      minify: false,
+      rollupOptions: {
+        output: {
+          assetFileNames: (assetInfo) => {
+            let extType = assetInfo.name.split('.')[1];
+
+            if (/png|jpeg|svg|gif|tiff|bmp|ico/i.test(extType)) {
+              extType = 'images';
+            } else if (/woff|woff2|ttf|otf/i.test(extType)) {
+              extType = 'fonts';
+            }
+
+            return `assets/${extType}/[name][extname]`;
+          },
+          entryFileNames: 'assets/js/[name].js',
+        },
+      },
     },
-    vite: {
-        build: {
-            cssMinify: false
-        }
-    }
+  },
+  // compressHTML: false,
 });
